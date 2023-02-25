51c87bdfc2abae3702fb9c9bc9c7aa97aef6191f
Brunno Manduca <brunno.manduca@ahgora.com.br>
2023-02-25 12:03:30 -0300

    Atualizando exercicios e aula 08

---
+++ Codes/scripts-python/exerc016.py
@@ -0,0 +1,11 @@
+"""
+    Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
+
+    Ex.: 
+        Digite um número: 6.124
+        A parte inteira do número 6.124 é 6.
+"""
+
+
+
+
---
+++ Codes/scripts-python/exerc021.py
@@ -0,0 +1,3 @@
+"""
+    Faça um programa em Python que abra e reproduza o audio de um arquivo MP3.
+"""
