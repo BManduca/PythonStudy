11cba681871d5a29ec5a638d721ebfb849a95a82
BManduca <brunnomanducarfe@gmail.com>
2023-08-13 22:02:57 -0300

    UPDATE EXERCISES 081 AO 083

--- Codes/scripts-python/Mundo03/Listas/exerc081.py
+++ Codes/scripts-python/Mundo03/Listas/exerc081.py
@@ -36,10 +36,15 @@ print('='*80)
 print(f'A LISTA DOS VALORES DIGITADOS: {valores}')
 print('='*80)
 print()
+print('='*60)
+print('{:=^60}'.format(' RESPOSTAS '))
+print('='*60)
+print()
+print('='*80)
 print(f'A) QUANTIDADE DE NÚMEROS DIGITADOS: {len(valores)}')
 print(f'B) A LISTA DE VALORES ORDENADA DE FORMA DECRESCENTE: {sorted(valores, reverse=True)}')
 if 5 in valores:
-    print('C) O VALOR 5 FAZ PARTE DA LISTA!')
+    print(f'C) O VALOR 5 FAZ PARTE DA LISTA!\nE A PRIMEIRA OCORRÊNCIA ESTÁ NA POSIÇÃO {valores.index(5)+1}')
 else:
     print('C) O VALOR 5 NÃO FAZ PARTE DA LISTA!')
-
+print('='*80)
