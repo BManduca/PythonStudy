34dfa8ff6347e0f19d6825add523ed2764316348
BManduca <brunnomanducarfe@gmail.com>
2023-10-08 21:50:00 -0300

    AULA 19 | UPDATE EXERCS 98 ao 100 finalizados

--- Codes/scripts-python/Mundo03/Funções/exerc100.py
+++ Codes/scripts-python/Mundo03/Funções/exerc100.py
@@ -7,3 +7,49 @@
     FUNÇÃO ANTERIOR.
 """
 
+from random import randint
+from time import sleep
+
+
+def mostraLinha():
+    print('-=' * 30)
+
+
+def mostraLinha2():
+    print()
+    print('~' * 80)
+    print()
+
+
+def sorteia(lista):
+    mostraLinha()
+    print()
+    print('   SORTEANDO 5 VALORES ==> |  ', end='')
+    for aux in range(0, 5):
+        n = randint(1, 10)
+        lista.append(n)
+        print(f'{n}  ', end='', flush=True)
+        sleep(0.5)
+    print('|', end='')
+    # print(f'   A LISTA SORTEADA ==> {lista}')
+    print('\n')
+    mostraLinha()
+    print()
+
+
+def somapar(lista):
+    soma = 0
+    listapar = []
+
+    for v in lista:
+        if v % 2 == 0:
+            listapar.append(v)
+            soma += v
+    mostraLinha2()
+    print(f' ==> A LISTA DE VALORES PARES: {listapar} \n\n ==> SOMA DOS VALORES PARES => {soma} ')
+    mostraLinha2()
+
+
+numeros = []
+sorteia(numeros)
+somapar(numeros)
