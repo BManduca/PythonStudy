2f9ab03454f60541edf48968bbd5e3207d840975
BManduca <brunnomanducarfe@gmail.com>
2023-10-14 21:28:20 -0300

    AULA 19 | UPDATE EXERCS 103, 104 e 105 finalizados

--- Codes/scripts-python/Mundo03/Funções/exerc104.py
+++ Codes/scripts-python/Mundo03/Funções/exerc104.py
@@ -6,3 +6,48 @@
 
     n = leiaInt('DIGITE UM VALOR PARA N: ')
 """
+
+
+def mostraLinha():
+    print('-' * 50)
+    print()
+
+
+def imprimir(text):
+    print('{:^50}'.format(text))
+
+
+def leiaInt(msg):
+
+    """
+        ==> IMPRIMIR MENSAGEM APÓS VALIDAR O VALOR INTEIRO INSERIDO PELO USUÁRIO.
+        :param msg: A MENSAGEM(NO CASO O VALOR) INSERIDO ATRAVÉS DO TELCADO PELO USUÁRIO.
+        :return: MENSAGEM IMPRESSA DIZENDO QUAL O VALOR O USUÁRIO INSERIU ATRAVÉS DO TECLADO.
+    """
+
+    ok = False
+    valor = 0
+
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31m ERRO!! FAVOR ISNERIR UM NÚMERO INTEIRO VÁLIDO! \033[m\n')
+        if ok:
+            break
+    return valor
+
+
+def mensagem(text):
+    return text
+
+
+# programa principal
+mostraLinha()
+numb = leiaInt('       ==> INSIRA UM VALOR INTEIRO: ')
+msg = mensagem(f'VOCÊ ACABOU DE DIGITAR O NÚMERO {numb} ')
+imprimir(msg)
+print()
+mostraLinha()
