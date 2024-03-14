1c5f128d06350d36e4df68783e7e3f735d89d353
BManduca <brunnomanducarfe@gmail.com>
2024-03-14 17:43:41 -0300

    develop: update project and apply more functions to delete itens and drop collection

--- Curso Python Para Dados SoulCode/Manipulação de dados com PyMongo/exercicio02_final_test_create_menu/create_menu_manipulate_data.py
+++ Curso Python Para Dados SoulCode/Manipulação de dados com PyMongo/exercicio02_final_test_create_menu/create_menu_manipulate_data.py
@@ -13,111 +13,210 @@ colors = (
 )
 
 
-def imprimirMsgEstilizada(msg, cor=0):
-    print(colors[cor], end='')
-    print(f'{msg:^60}')
-    print(colors[0], end='')
+"""
+    - Crie um script contendo cinco funções:
+        - Criar itens, onde o usuário pode selecionar a quantidade de campos => OK
+        - Mostrar todos os documentos => OK
+        - Deletar documentos pelo ID => 
+        - Deletar toda a coleção do banco de dados => 
+        - Atualizar documentos por ID ou por campos => 
+"""
+    
+def messageCentralized(msg, wid):
+    tam = len(msg)
+    space = (wid - tam) // 2
+    text = ' ' * space + msg + ' ' * space
+    # Se o texto não puder ser centralizado perfeitamente, ajuste uma posição para a direita
+    if len(text) < wid:
+        text += ' '
+        
+    return text
 
-def imprimirMsg(msg):
-    print(f'{msg}')
+def printTextCentralized(msg, wid, cor=0):
+    message = messageCentralized(msg, wid)
+    print(colors[cor], end='')
+    print(message)
+    print(colors[0], end='')
 
 def imprimirLinha20(cor=0):
     print(colors[cor], end='')
     print('-=-'*20)
     print(colors[0], end='')
 
-def imprimirLinha(cor=0):
+def imprimirLinha28(cor=0):
     print(colors[cor], end='')
-    print('-=-'*5)
+    print('-=-'*28)
     print(colors[0], end='')
 
 def get_database():
     from pymongo import MongoClient
-    import pymongo
 
     CONNECTION_STRING = "mongodb+srv://manduca:xmd8Xvu1XCcoIz6Q@mydatabase.8vdqilo.mongodb.net/"
 
     client = MongoClient(CONNECTION_STRING)
 
     print(' ')
-    imprimirLinha20(2)
-    imprimirMsgEstilizada('CONECTADO AO BANCO COM SUCESSO!',2)
-    imprimirLinha20(2)
+    imprimirLinha28(2)
+    printTextCentralized('EFETUANDO CONEXÃO COM O BANCO...', 84, 2)
+    time.sleep(2)
+    printTextCentralized('>>>>', 84, 2)
+    time.sleep(2)
+    printTextCentralized('>>', 84, 2)
+    time.sleep(2)
+    printTextCentralized('>', 84, 2)
+    time.sleep(2)
+    printTextCentralized('CONECTADO AO BANCO COM SUCESSO!', 84, 2)
+    imprimirLinha28(2)
     print(' ')
     time.sleep(2)
     system('clear')
 
     return client['exerciseDatabase']
 
-def exibir_menu():
-    imprimirLinha20(3)
-    imprimirMsgEstilizada('BEM-VINDO AO PROGRAMA DE MANIPULAÇÃO DE DADOS!', 3)
-    imprimirMsgEstilizada('SELECIONE UMA OPÇÃO: ', 3)
+def showMenu():
+    imprimirLinha28(3)
+    printTextCentralized('BEM-VINDO AO PROGRAMA DE MANIPULAÇÃO DE DADOS!', 80, 3)
+    printTextCentralized('SELECIONE UMA OPÇÃO: ', 80, 3)
     print(' ')
-    imprimirMsgEstilizada('1 => CADASTRO DE DOCUMENTO', 3)
-    imprimirMsgEstilizada('2 => MOSTRAR TODOS OS DOCUMENTOS', 3)
-    imprimirMsgEstilizada('0 => FINALIZAR PROGRAMA', 3)
-    imprimirLinha20(3)
-    # imprimirMsgEstilizada('2 => ')
-    # imprimirMsgEstilizada('3 => ')
+    printTextCentralized('1 => CADASTRO DE DOCUMENTO', 80, 3)
+    printTextCentralized('2 => MOSTRAR TODOS OS DOCUMENTOS', 80, 3)
+    printTextCentralized('3 => DELETAR UM DOCUMENTO ATRAVÉS DO ID', 80, 3)
+    printTextCentralized('4 => DELETAR TODA A COLEÇÃO DO BANCO DE DADOS', 80, 3)
+    printTextCentralized('0 => FINALIZAR PROGRAMA', 80, 3)
+    imprimirLinha28(3)
 
-def cadastrarDocumento():
+
+"""
+This Python function allows the user to input a specified number of key-value pairs to create a
+document and store it in a MongoDB collection.
+"""
+def registerDocument():
     dbname = get_database()
     collection_name = dbname['exerciseListDatabase']
     qtd = int(input('ENTRE COM A QUANTIDADE DE CAMPOS QUE O DOCUMENTO TERÁ: '))
     dct = dict(input('DIGITE A CHAVE E O VALOR SEPARADO POR ESPAÇOS: ').split() for _ in range(qtd))
 
     print(' ')
-    imprimirLinha20(2)
-    imprimirMsgEstilizada(f'{dct}',2)
-    imprimirLinha20(2)
+    imprimirLinha28(2)
+    printTextCentralized(f'{dct}', 84,2)
+    imprimirLinha28(2)
     collection_name.insert_one(dct)
+    
+    time.sleep(2)
 
     print(' ')
-    imprimirLinha20(2)
-    imprimirMsgEstilizada('DADOS CADASTRADOS COM SUCESSO!', 2)
-    imprimirLinha20(2)
+    imprimirLinha28(2)
+    printTextCentralized('DADOS CADASTRADOS COM SUCESSO!', 84, 2)
+    imprimirLinha28(2)
     print(' ')
     time.sleep(5)
     system('clear')
 
-def mostrarDocumentos():
+
+"""
+This Python function retrieves and displays documents from a MongoDB collection with a styled
+message for each item, and then clears the screen after a delay.
+"""
+def showDocuments():
     dbname = get_database()
     collection_name = dbname['exerciseListDatabase']
     detalhes_items = collection_name.find()
     print(' ')
 
-    imprimirLinha20()
+    imprimirLinha28()
     for item in detalhes_items:
-        imprimirMsgEstilizada(f'{item}')
-    imprimirLinha20()
+        printTextCentralized(f'{item}', 84)
+    imprimirLinha28()
     print(' ')
 
     time.sleep(8)
     system('clear')
 
 
+def deleteDocumentForID():
+    dbname = get_database()
+    collection_name = dbname['exerciseListDatabase']
+    deleteID = str(input('ENTRE COM O ID DO DOCUMENTO QUE SERÁ DELETADO: '))
+    
+    collection_name.delete_one({"_id": deleteID})
+    
+    time.sleep(2)
+    
+    print(' ')
+    imprimirLinha28(1)
+    printTextCentralized(f'DOCUMENTO {deleteID} FOI DELETADO COM SUCESSO!', 80, 1)
+    imprimirLinha28(1)
+    print(' ')
+    time.sleep(5)
+    system('clear')
+
+
+def deleteAllCollectionDB():
+    dbname = get_database()
+    collection_name = dbname['exerciseListDatabase']
+    
+    answer = str(input('DESEJA REALMENTE DELETAR TODA A COLEÇÃO DO BANCO DE DADOS? ')).upper()
+    time.sleep(2)
+    
+    while answer not in 'SN':
+        answer = str(input('DESEJA REALMENTE DELETAR TODA A COLEÇÃO DO BANCO DE DADOS? ')).upper()
+        time.sleep(2)
+    print(' ')
+    
+    if answer == 'N':
+        time.sleep(2)
+        printTextCentralized('RETORNANDO PARA O MENU PRINCIPAL..', 80, 2)
+        print()
+        printTextCentralized('3', 80, 2)
+        time.sleep(2)
+        printTextCentralized('2', 80, 2)
+        time.sleep(2)
+        printTextCentralized('1', 80, 2)
+        time.sleep(2)
+        system('clear')        
+    else:
+        collection_name.drop()
+        printTextCentralized('DELETANDO COLEÇÃO...')
+        print()
+        printTextCentralized('3', 80, 2)
+        time.sleep(2)
+        printTextCentralized('2', 80, 2)
+        time.sleep(2)
+        printTextCentralized('1', 80, 2)
+        time.sleep(2)
+        printTextCentralized('COLEÇÃO DELETADA COM SUCESSO!', 80, 2)
 
 
 def main():
     while True:
-        exibir_menu()
+        showMenu()
         print( )
-        theChoice = input('INSIRA A OPÇÃO DESEJADA: ')
+        
+        textChoice = 'INSIRA A OPÇÃO DESEJADA: '
+        
+        theChoice = input(f'{textChoice}')
 
         if theChoice == '1':
-            cadastrarDocumento()
+            registerDocument()
         elif theChoice == '2':
-            mostrarDocumentos()
+            showDocuments()
+        elif theChoice == '3':
+            deleteDocumentForID()
+        elif theChoice == '4':
+            deleteAllCollectionDB()
         elif theChoice == '0':
             print('\n')
-            imprimirLinha20(1)
-            imprimirMsgEstilizada('FINALIZANDO PROGRAMA!', 1)
-            imprimirLinha20(1)
+            imprimirLinha28(1)
+            printTextCentralized('FINALIZANDO PROGRAMA!', 84, 1)
+            imprimirLinha28(1)
             print('\n')
             break
         else:
-            print('OPÇÃO INVÁLIDA! POR FAVOR, INSIRA NOVAMENTE A OPÇÃO ESCOLHIDA!')
+            print('\n')
+            print('OPÇÃO INVÁLIDA! POR FAVOR,\nINSIRA NOVAMENTE UMA OPÇÃO DO MENU!')
+            print('\n')
+            time.sleep(5)
+            system('clear')
 
 
 if __name__ == "__main__":
